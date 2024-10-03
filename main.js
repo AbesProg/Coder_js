1ba77e430da327b7b809d72ecd4c467ceeb10d64
Sebastian Campos <seba1415campos@gmail.com>
2024-10-03 17:26:08 -0300

    registrador por consola

--- main.js
+++ main.js
@@ -1,51 +1,49 @@
 
-
-function cancelar_juego() {
-    return true;
+function ingresarNota(nombreMateria) {
+    let nota = null;
+    while (true) {
+        nota = prompt(`Ingrese la nota de ${nombreMateria}`);
+        if (nota === null) {
+            salir();a
+            return; 
+        }
+        nota = Number(nota);
+        if (!isNaN(nota) && nota >= 1 && nota <= 10) {
+            return nota;
+        }
+        alert("Ingrese una nota válida");
+    }
 }
 
-
-function jugar() {
-
-    let intentos = 0;
-    let nombre = prompt('Ingrese su nombre');
-    if (nombre === null) {
-        return cancelar_juego();
-    }
-    let numero_random = Math.ceil(Math.random() * 20);
-
-
-    while (!nombre || !isNaN(nombre)) {
-        nombre = prompt('Ingrese un nombre válido');
-        if (nombre === null) {
-            return cancelar_juego();
+function pasar_notas() {
+    while (true) {
+        let nombre_alumno = prompt('Ingrese el nombre del alumno');
+        if (nombre_alumno === null) {
+            return;
         }
-    }
+        while (!nombre_alumno || !isNaN(nombre_alumno)) {
+            nombre_alumno = prompt("Ingrese un nombre válido");
+        }
 
-    while (intentos < 3) {
-
-        let numero_usuario = Number(prompt('Ingrese un numero entre 1 y 20'));
-
-        if (numero_usuario < 1 || numero_usuario > 20 || isNaN(numero_usuario)) {
-            alert('Ingrese un numero válido entre 1 y 20');
-            if (numero_usuario === null) {
-                return cancelar_juego();
+        let alumno = {
+            nombre: nombre_alumno,
+            materias: {
+                matematica: ingresarNota('Matemática'),
+                ingles: ingresarNota('Inglés'),
+                biologia: ingresarNota('Biología'),
+                computacion: ingresarNota('Computación'),
+                historia: ingresarNota('Historia')
             }
-        } else if (numero_usuario == numero_random) {
-            alert('¡Felicitaciones ' + nombre + ', GANASTE!');
-            break;
-        } else if (numero_usuario !== numero_random) {
-            alert('Seguí participando');
-            intentos++;
-        } else {
-            alert('se acabaron los intentos. El número era ' + numero_random);
+        };
+
+        alumnos.push(alumno);
+
+        if (!confirm("¿Desea ingresar otro alumno?")) {
+            break; 
         }
     }
 
-    let jugar_nuevamente = confirm('¿Queres volver a jugar?');
-    if (jugar_nuevamente) {
-        jugar();
-    }
+    console.log(alumnos);
 }
-jugar();
+
 
